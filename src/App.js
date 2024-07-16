1b7c15c100a29b52294b917ad607768f2d243480
juanEulogio <eulogiomarti@wisc.edu>
2024-07-16 16:18:36 -0500

    lottie starting animation feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Lottie from 'lottie-web';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. This is a new change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [displayStatus, setStatus]= useState("lottieStart")
+  const container= useRef(null)
+
+
+  useEffect(() => {
+
+    const instance= Lottie.loadAnimation({
+      container: container.current,
+      loop: false,
+      renderer: `svg`,
+      autoplay: true,
+      animationData: require("./lotties/startingAnime.json")
+    })
+
+    //on animation complete event
+    instance.addEventListener('complete', function(e) {
+      setStatus("menu");
+      instance.destroy()
+    });
+
+    // implemented Cleanup function to destroy the animation instance, in order to
+    // remove lottie's ui library bug on react that renders more than one animation on the DOM
+    return () => instance.destroy();
+  }, [])
+
+
+  return (<div>
+  
+    {displayStatus=== "lottieStart" ?
+
+      <div className='centerContent' ref={container} style={{width:"100%"}}></div>
+    :
+      
+      <div className='fade-in'>
+        <div className='centerContent'>
+
+            <h1 >hello</h1>
+
+        </div>
+      </div>
+    
+
+    }</div>);
 }
 
 export default App;
