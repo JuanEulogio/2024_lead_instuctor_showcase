508d70d90a98f86515f1ab327c8d8354ef969901
juanEulogio <eulogiomarti@wisc.edu>
2024-07-19 23:23:15 -0500

    UpatedLayout: better tailwind code and readability

--- src/App.js
+++ src/App.js
@@ -30,8 +30,6 @@ function App() {
 
     // implemented Cleanup function to destroy the animation instance, in order to
     // remove lottie's ui library bug on react that renders more than one animation on the DOM
-    console.log("passing 1.1")
-
     return () => instance.destroy();
   }, [])
 
@@ -47,9 +45,7 @@ function App() {
 
     // implemented Cleanup function to destroy the animation instance, in order to
     // remove lottie's ui library bug on react that renders more than one animation on the DOM
-    console.log("passing 1.2")
     return () => instance.destroy();
-    
   }, [displayStatus])
 
   useEffect(() => {
@@ -63,8 +59,6 @@ function App() {
 
     // implemented Cleanup function to destroy the animation instance, in order to
     // remove lottie's ui library bug on react that renders more than one animation on the DOM
-    console.log("passing 1.3")
-
     return () => instance.destroy();
 
   }, [displayStatus])
@@ -75,48 +69,41 @@ function App() {
     {displayStatus=== "lottieStart" ?
 
       <div className='grid h-screen place-items-center' ref={containerInto}></div>
+    
     :
       
-    <div className='animate-fadeIn p-4 pt-24'>
-      <Reveal><h1 className='flex justify-center text-center font-Patrick-Hand font-bold text-6xl pb-56 lg:pb-0'>Students Website Showcase</h1></Reveal>
+    <div className='p-4 md:px-20 xl:px-32 pt-24'>
+      <Reveal><h1 className='flex justify-center text-center font-Patrick-Hand font-bold text-6xl pb-56'>Students Website Showcase</h1></Reveal>
 
-      <div className='grid gap-y-96 lg:grid-cols-4 grid-rows-2 place-items-center'>
+      <div className='grid gap-y-72 place-items-center'>
 
-          <div className='lg:order-2 2xl:pt-44' >
+          <div className='grid lg:order-2 gap-4 gap-y-72 lg:grid-cols-2 lg:justify-self-start' >
             <Reveal><Card img={"pointer"} title={"Exotic Car Shop"} header={"Card Header"} description={"Card Description"} link={"https://github.com/TwinkieMan22/Exotic-car-Market"} ></Card></Reveal>
+            <Reveal><Card img={"blogging"} title={"Blog Website"} header={"Card Header"} description={"Card Description"} link={"https://github.com/Chase-Nickel/react-blog"}></Card></Reveal>
           </div>
 
-          <div className='lg:order-1 grid justify-items-center lg:col-start-3 lg:col-span-2 lg:row-span-2 lg:-translate-y-36 lg:max-w-[80%]'>
+          <div className='grid lg:order-1 lg:justify-self-end lg:max-w-[55%] xl:max-w-[40%]'>
             <Reveal>
-              <div className='bg-pastelBeige p-4 lg:p-8 rounded-3xl'><img src={studentFeedback} style={{borderRadius:"1.5rem"}}></img></div>
-              <div className='w-56 -rotate-45 lg:-translate-x-32 2xl:-translate-x- translate-y-20' ref={containerArrow}></div>
-            </Reveal>
-          </div>
-
-          
-          <div className='order-3 2xl:pt-44' >
-            <Reveal><Card img={"blogging"} title={"Blog Website"} header={"Card Header"} description={"Card Description"} link={"https://github.com/Chase-Nickel/react-blog"}></Card>
+              <div className='bg-pastelBeige justify-self-center p-4 lg:p-6 rounded-3xl'><img src={studentFeedback} style={{borderRadius:"1.5rem"}}></img></div>
+              <div className='w-40 md:w-52 -rotate-45' ref={containerArrow}></div>
             </Reveal>
           </div>
             
           
-          <div className='order-4 grid lg:col-span-2 lg:row-span-2 lg:pt-56 md:max-w-[60%] 2xl:max-w-[50]'>
-            <Reveal><div ref={containerTeaching}></div></Reveal>
-          </div>
-
-          <div className='order-5 lg:col-start-3 lg:pt-44'>
+          <div className='grid order-3 gap-y-72 lg:grid-cols-2 gap-4 lg:justify-self-end'>
             <Reveal><Card img={"personalWeb"} title={"Personal Website"} header={"Card Header"} description={"Card Description"} link={"https://github.com/NachoWizard64/2024SummerProject"}></Card></Reveal>
-          </div>
-            
-          <div className='order-6 lg:pt-44'>
             <Reveal><Card img={"hobbyWeb"} title={"Hobby Website"} header={"Card Header"} description={"Card Description"} link={"https://github.com/MUINarku/jenkin"}></Card></Reveal>
           </div>
 
-      </div>
-    </div>  
-    
+          <div className='order-4 lg:justify-self-start lg:max-w-[50%]'>
+            <Reveal><div ref={containerTeaching}></div></Reveal>
+          </div>
 
-    }</div>);
+      </div>
+    </div>
+    }
+    
+  </div>);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,7 @@ module.exports = {
     "./src/components/*.js"
   ],
   theme: {
-    extend: {
-      // that is animation class
-      animation: {
-        fadeIn: 'fadeIn 1s',
-      },
-
-      // that is actual animation
-      keyframes: theme => ({
-        fadeIn: {
-          '0%': {
-            opacity: 0
-          },
-          '100%': {
-            opacity: 100
-          },
-        },
-      }),
-    },
-
+    extend: {},
     cursor: {
       drawn: "url(/src/assets/cursor.png), default",
     },
